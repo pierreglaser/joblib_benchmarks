224061e2c5fc83884f4f2984f4650070415a1afe
tomMoral <thomas.moreau.2010@gmail.com>
2018-09-24 17:58:13 +0200

    ENH improve run_old_release.sh

--- run_old_releases.sh
+++ run_old_releases.sh
@@ -2,32 +2,29 @@
 
 # first get the tags from the joblib repository
 
-if cd "$JOBLIB_PATH"; then
-	echo "listing tags from joblib..."
-else
+if [ -z "$JOBLIB_PATH" ] || [ ! -d "$JOBLIB_PATH" ]; then
 	echo "JOBLIB_PATH is either not set or
 	pointing to a wrong location, aborting operation."
+	return
+else
+	pushd $JOBLIB_PATH
+	echo "listing tags from joblib..."
 fi
 
 # we start bechmarking releases when joblib is written in
 # python3, e.g approximately v0.8.0
-OLDEST_BENCHMARKED_RELEASE="0.12.5"
+OLDEST_BENCHMARKED_RELEASE="0.8.0"
 
 # this lists the tags which history contains the
 # $OLDEST_BENCHMARKED_RELEASE, e.g tags newer than
 # $OLDEST_BENCHMARKED_RELEASE
-ALL_TAGS=$(git tag --sort=committerdate --contains $OLDEST_BENCHMARKED_RELEASE)
+ALL_TAGS=$(git tag --sort=committerdate --contains "$OLDEST_BENCHMARKED_RELEASE")
 echo "asv will benchmark the following tags: 
 $ALL_TAGS"
 
 # we now move to the joblib_benchmarks repository and start
 # running the bechmarks
-if cd "$JOBLIB_BENCHMARKS_PATH"; then 
-	echo "now moving to joblib_benchmarks directory..."
-else 
-	echo "JOBLIB_BENCHMARKS_PATH is either not set or 
-	pointing to a wrong location, aborting operation."
-fi
+popd
 
 for tag in $ALL_TAGS; do
 	echo "running asv for tag $tag"
