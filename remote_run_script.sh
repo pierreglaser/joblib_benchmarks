94f0959ed3f0405da4f18c86b677a3e745a652f7
Pierre Glaser <pierreglaser@msn.com>
2018-09-26 15:16:41 +0200

    benchmarks are run in /tmp

--- remote_run_script.sh
+++ remote_run_script.sh
@@ -7,18 +7,18 @@ set -o xtrace
 # repository, so that there exists a launch script doing:
 # "git clone joblib_bencmarks && cd joblib_benchmarks"
 # "source remote_run_script.sh"
+JOBLIB_BENCHMARK_LOCATION=$PWD
 
-# all repositories are created in the parent directory of joblib_benchmarks
-cd "${HOME}"
+# all repository, python distributions and benchmaks will be ran here
+BENCHMARKS_TEMP_DIR="/tmp/benchmark_temp_dir"
+mkdir "${BENCHMARKS_TEMP_DIR}"
+cd "${BENCHMARKS_TEMP_DIR}"
 
 # first, we install python using miniconda
 # this version comes with python3.6
 wget https://repo.continuum.io/miniconda/Miniconda3-4.3.11-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p "${HOME}/miniconda"
-export PATH="${HOME}/miniconda/bin:${PATH}"
-
-mkdir temp_folder
-cd temp_folder
+bash miniconda.sh -b -p "${BENCHMARKS_TEMP_DIR}/miniconda"
+export PATH="${BENCHMARKS_TEMP_DIR}/miniconda/bin:${PATH}"
 
 echo "creating a python3.6 virtualenv..."
 conda create -y -n joblib_benchmarks_virtualenv pip
@@ -32,7 +32,7 @@ echo "...done. Cloning joblib..."
 git clone https://github.com/joblib/joblib
 
 echo "...done. Copying joblib_benchmarks..."
-cp -r "${HOME}/joblib_benchmarks" joblib_benchmarks
+cp -r "${JOBLIB_BENCHMARK_LOCATION}" joblib_benchmarks
 
 echo "...done. Installing asv..."
 pip install asv
