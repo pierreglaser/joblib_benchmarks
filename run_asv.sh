d7fc52390d56333dc1aff938044cf17d39e989ec
Pierre Glaser <pierreglaser@msn.com>
2019-06-23 18:42:19 +0200

    add asv bash script

---
+++ run_asv.sh
@@ -0,0 +1,20 @@
+branches='use-most-recently-sent-batch synchronize-logging-and-logic-ops use-time-spent-in-worker default-batching-strategy default-slow-increase default-slow-increase-fast-decrease most-recent-batch-slow-increase most-recent-batch-slow-increase-fast-decrease most-recent-batch-slow-increase-fast-decrease-end-to-end-duration'
+
+for branch in $branches; do
+	asv run -e -b bench_auto_batching.AutoBatchingSuite.track_ "${branch}"^!
+done
+
+# for branch in default-slow-increase most-recent-batch-slow-increase; do
+# 	asv run -e -b bench_auto_batching.AutoBatchingSuite.track_c ${branch}^!
+# done
+# for branch in most-recent-batch-slow-increase-fast-decrease; do
+# 	asv run -e -b bench_auto_batching.AutoBatchingSuite.track_c ${branch}^!
+# done
+
+# for branch in most-recent-batch-slow-increase-fast-decrease-end-to-end-duration; do
+# 	asv run -e -b bench_auto_batching.AutoBatchingSuite.track_c ${branch}^!
+# done
+
+# for branch in default-batching-strategy; do
+# 	asv run -e -b bench_auto_batching.AutoBatchingSuite.track_l ${branch}^!
+# done
